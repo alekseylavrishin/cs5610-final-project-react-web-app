b69cc6551c51f1d73df6d31579235d6e0cb28545
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2023-12-11 08:02:11 -0500

    Removed baseURL in follows/client.js and created URL const outside of axios.create command

--- src/Project/follows/client.js
+++ src/Project/follows/client.js
@@ -4,25 +4,27 @@ const client = axios.create({
     withCredentials: true,
     //baseURL: process.env.REACT_APP_API_URL
     //baseURL: "http://localhost:4000/api/users"
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api/users"
+    //baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api/users"
 });
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api/users";
+
 
 export const userFollowsUser = async (followed) => {
-    const response = await client.post(`/${followed}/follows`);
+    const response = await client.post(`${API_URL}/${followed}/follows`);
     return response.data;
 };
 
 export const userUnfollowsUser = async (followed) => {
-    const response = await client.delete(`/${followed}/follows`);
+    const response = await client.delete(`${API_URL}/${followed}/follows`);
     return response.data;
 };
 
 export const findFollowersOfUser = async (followed) => {
-    const response = await client.get(`/${followed}/followers`);
+    const response = await client.get(`${API_URL}/${followed}/followers`);
     return response.data;
 };
 
 export const findFollowedUsersByUser = async (follower) => {
-    const response = await client.get(`/${follower}/following`);
+    const response = await client.get(`${API_URL}/${follower}/following`);
     return response.data;
 };
